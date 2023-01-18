477483a1fd497642975fd91791b2b648bfc4f878
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-19 00:22:27 +0100

    Added the links in the header nav through an array and changed the col size

--- src/components/header/HeaderNav.vue
+++ src/components/header/HeaderNav.vue
@@ -1,6 +1,19 @@
 <script>
 export default {
     name: 'HeaderNav',
+
+    data() {
+        return {
+            navLinksList: [
+                'Home',
+                'Courses',
+                'Instructor',
+                'Events',
+                'Pages',
+                'Elements',
+            ],
+        }
+    }
 }
 </script>
 
@@ -8,24 +21,22 @@ export default {
 <template>
     <nav class="container-fluid">
         <div class="row align-items-center justify-content-between">
-            <div class="col-3">
+            <div class="col-2">
                 <a class="my_logo-container">
                     <img src="../../assets/img/logo-light.png" alt="Logo" class="w-100">
                 </a>
             </div>
 
-            <div class="col-6">
+            <div class="col-8">
                 <ul class="list-unstyled d-flex m-0 justify-content-between">
-                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
+                    <li v-for="navLink in navLinksList"><a href="#" class="text-uppercase text-decoration-none">{{
+                        navLink
+                    }}</a>
+                    </li>
                 </ul>
             </div>
 
-            <div class="col-3">
+            <div class="col-1">
                 <div class="my_icons-container d-flex justify-content-between">
                     <span>iii</span>
                     <span>iii</span>
