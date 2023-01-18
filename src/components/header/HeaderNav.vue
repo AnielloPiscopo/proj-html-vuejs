1f8258a66040d2a94c77b187726bec18839def0b
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-18 17:27:58 +0100

    Improved the header nav

--- src/components/header/HeaderNav.vue
+++ src/components/header/HeaderNav.vue
@@ -6,19 +6,33 @@ export default {
 
 
 <template>
-    <nav>
-        <a class="my_logo-container">
-            <img src="../../assets/img/logo-light.png" alt="Logo">
-        </a>
+    <nav class="container-fluid">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-3">
+                <a class="my_logo-container">
+                    <img src="../../assets/img/logo-light.png" alt="Logo" class="w-100">
+                </a>
+            </div>
 
-        <ul class="list-unstyled d-flex">
-            <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-            <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-            <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-            <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-            <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-            <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
-        </ul>
+            <div class="col-6">
+                <ul class="list-unstyled d-flex m-0 justify-content-between">
+                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
+                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
+                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
+                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
+                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
+                    <li><a href="#" class="text-uppercase text-decoration-none">Home</a></li>
+                </ul>
+            </div>
+
+            <div class="col-3">
+                <div class="my_icons-container d-flex justify-content-between">
+                    <span>iii</span>
+                    <span>iii</span>
+                    <span>iii</span>
+                </div>
+            </div>
+        </div>
     </nav>
 </template>
 
