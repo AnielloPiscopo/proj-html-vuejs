2eff0457c44b786056e4745656fe4cf693ac370c
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-18 18:12:42 +0100

    Modified the footer

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -1,12 +1,23 @@
 <script>
+import FooterUpperPart from './FooterUpperPart.vue';
+import FooterLowerPart from './FooterLowerPart.vue';
+
 export default {
     name: 'AppFooter',
+
+    components: {
+        FooterUpperPart,
+        FooterLowerPart,
+    }
 }
 </script>
 
 
 <template>
-    <footer></footer>
+    <footer class="p-5">
+        <FooterUpperPart />
+        <FooterLowerPart />
+    </footer>
 </template>
 
 
---
+++ src/components/footer/FooterUpperPart.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'FooterUpperPart',
+}
+</script>
+
+<template>
+    <div class="container-fluid">
+        <div class="row justify-content-between">
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-4" v-for="shish in 3">
+                        <ul class="list-unstyled">
+                            <li>shish</li>
+                            <li>shish</li>
+                            <li>shish</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
