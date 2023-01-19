b50c082f019d72d450863a0248635da11a9e884d
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-19 12:35:55 +0100

    Fixed the MainTestimonials component

--- src/components/main/MainTestimonials.vue
+++ src/components/main/MainTestimonials.vue
@@ -13,22 +13,26 @@ export default {
 </script>
 
 <template>
-    <div class="container-fluid d-flex flex-column">
-        <div class="my_img-container rounded rounded-pill align-self-center"></div>
-        <p class="text-center">
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas repellendus voluptatibus nemo vero nisi
-            cum, nostrum dolorem corrupti. Dignissimos iusto quos repellendus consectetur ducimus totam beatae? Nam
-            enim nihil voluptas recusandae impedit neque fuga magnam animi! Nesciunt, nam!"
-        </p>
-    </div>
+    <section id='testimonials' class="p-5">
+        <div class="container-fluid d-flex flex-column p-5 my-5 text-center">
+            <img :src="store.getImgPath('testimonials-standard-2.png')" alt="" class="align-self-center">
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas repellendus voluptatibus nemo
+                        vero nisi
+                        cum, nostrum dolorem corrupti. Dignissimos iusto quos repellendus consectetur ducimus totam
+                        beatae? Nam
+                        enim nihil voluptas recusandae impedit neque fuga magnam animi! Nesciunt, nam!"
+                    </p>
+                </div>
+            </div>
+            <h3 class="my_font-serif ">Joan Collins</h3>
+            <span class="text-uppercase ">Student</span>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
-.my_img-container {
-    background-image: url('../../assets/img/instructor-img-6-150x150.jpg');
-    height: 100px;
-    width: 100px;
-    background-position: top;
-    background-position: 60% 25%;
-}
+
 </style>
