e6d728a9860306ce95deaa075d3d6742c328df83
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-19 23:49:28 +0100

    Completed MainClientsLogoContainer component

--- src/components/main/AppMain.vue
+++ src/components/main/AppMain.vue
@@ -5,6 +5,7 @@ import MainTestimonials from './MainTestimonials.vue';
 import MainProgrammDetails from './MainProgrammDetails.vue';
 import MainCourses from './MainCourses.vue';
 import MainPricingPlans from './MainPricingPlans.vue';
+import MainClientsLogoContainer from './MainClientsLogoContainer.vue';
 
 export default {
     name: 'AppMain',
@@ -15,6 +16,7 @@ export default {
         MainProgrammDetails,
         MainCourses,
         MainPricingPlans,
+        MainClientsLogoContainer,
     }
 }
 </script>
@@ -28,6 +30,7 @@ export default {
         <MainProgrammDetails />
         <MainCourses />
         <MainPricingPlans />
+        <MainClientsLogoContainer />>
     </main>
 </template>
 
--- src/components/main/MainClientsLogoContainer.vue
+++ src/components/main/MainClientsLogoContainer.vue
@@ -15,9 +15,26 @@ export default {
 </script>
 
 <template>
-    <div id="clients" class="container-fluid">
-        <div class="row">
-
+    <div id="clients" class="p-5">
+        <div class="container-fluid py-5">
+            <div class="row">
+                <div class="col-3">
+                    <img :src="store.getImgPath(clientsLogoImgPath + '5' + clientsLogoImgExtension)" alt=""
+                        class="w-100">
+                </div>
+                <div class="col-3">
+                    <img :src="store.getImgPath(clientsLogoImgPath + '1' + clientsLogoImgExtension)" alt=""
+                        class="w-100">
+                </div>
+                <div class="col-3">
+                    <img :src="store.getImgPath(clientsLogoImgPath + '2' + clientsLogoImgExtension)" alt=""
+                        class="w-100">
+                </div>
+                <div class="col-3">
+                    <img :src="store.getImgPath(clientsLogoImgPath + '1' + clientsLogoImgExtension)" alt=""
+                        class="w-100">
+                </div>
+            </div>
         </div>
     </div>
 </template>
